e6d2aa23b7509bab54a18a04ed47c74a503fbc0d
rafaelmacielrsm <faelrrrr@gmail.com>
2017-08-13 20:16:40 -0300

    Setups the infection-report serializer

--- app/serializers/infection_report_serializer.rb
+++ app/serializers/infection_report_serializer.rb
@@ -1,3 +1,5 @@
 class InfectionReportSerializer < ActiveModel::Serializer
   attributes :id
+  has_one :survivor
+  belongs_to :infected_survivor, {class_name: 'Survivor', foreign_key: 'infected_id'}
 end
--- app/serializers/survivor_serializer.rb
+++ app/serializers/survivor_serializer.rb
@@ -1,10 +1,9 @@
 class SurvivorSerializer < ActiveModel::Serializer
   attributes :id, :name, :age, :gender, :latitude, :longitude, :inventory
-  # has_many :items, serializer: ItemSerializer
 
   # This method organize the items association so it is more intuitivily
   # serialized
-  # Output look like:  { water: 0, food: 1, medication: 9, ammunition: 8 }
+  # The Output looks like:  { water: 0, food: 1, medication: 9, ammunition: 8 }
   def inventory
     object.items.collect { |item|
       [Inventory.lookup(item.item_id), item.quantity]
