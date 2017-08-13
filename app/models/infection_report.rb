05fbadec830a7fd86dc0633c51899143c013811c
rafaelmacielrsm <faelrrrr@gmail.com>
2017-08-13 20:17:43 -0300

    Adds a callback to flag infected survivors

--- app/models/infection_report.rb
+++ app/models/infection_report.rb
@@ -1,4 +1,6 @@
 class InfectionReport < ApplicationRecord
+  after_create :check_reported_survivor
+
   include ActiveModel::Validations
   validates_with BlockSelfReportValidator
   validates_uniqueness_of :infected_id, {
@@ -8,4 +10,11 @@ class InfectionReport < ApplicationRecord
   belongs_to :survivor
   belongs_to :reported_survivor,
     class_name: "Survivor", foreign_key: 'infected_id'
+
+  def check_reported_survivor
+    reported = self.reported_survivor
+    unless reported.infected?
+      reported.update(infected: true) if reported.reported_by.count >= 3
+    end
+  end
 end
